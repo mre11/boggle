f820e4bbc385d0acdceeca3670e397db5413c403
Morgan Empey <morgan.empey@gmail.com>
2016-03-20 14:06:03 -0600

    Started creating a rough GUI.

--- PS8/BoggleClient/GameBoard.Designer.cs
+++ PS8/BoggleClient/GameBoard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BoggleClient
 {
-    partial class Form1
+    partial class GameBoard
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // GameBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 469);
+            this.Name = "GameBoard";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
