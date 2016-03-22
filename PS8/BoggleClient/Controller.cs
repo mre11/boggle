58a57a2fd86ca9f905d4a9dba30fbd898d88def2
Morgan Empey <morgan.empey@gmail.com>
2016-03-21 22:25:15 -0600

    Join Game request is now working.

--- PS8/BoggleClient/Controller.cs
+++ PS8/BoggleClient/Controller.cs
@@ -29,27 +29,29 @@ namespace BoggleClient
         public Controller(IBoggleBoard game, StartForm start)
         {
             startWindow = start;
-            startWindow.JoinGameEvent += JoinGame;
+            startWindow.JoinGameEvent += StartNewGame;
             startWindow.CancelEvent += CancelGameSearch;
 
-            gameWindow = game;            
+            gameWindow = game;
             gameWindow.PlayWordEvent += PlayWord;
-            gameWindow.ExitGameEvent += ExitGame;            
+            gameWindow.ExitGameEvent += ExitGame;
         }
 
-        private async void JoinGame()
+        private async void StartNewGame()
         {
+            // Create a new user
             string userToken = await CreateUser(startWindow.PlayerName);
 
-            HttpClient client = CreateClient(startWindow.ServerUrl);
+            // Join a game
+            string gameID = await JoinGame(userToken, startWindow.RequestedDuration);
+
+            // TODO Update the game board using Game Status request before showing the window
 
 
-            // Try connecting to the boggle server
-            bool success = true;
+            bool success = userToken != "" && gameID != "";
 
             if (success)
             {
-                // TODO populate the game window using the game status
                 startWindow.Hide();
                 gameWindow.OpenWindow();
             }
@@ -58,45 +60,7 @@ namespace BoggleClient
                 // Display error message telling the user they've entered the wrong Boggle Server.
                 // TODO also catch the case that they haven't entered a player name!
                 startWindow.DisplayErrorMessage();
-            }            
-        }
-
-        private async void CancelGameSearch()
-        {
-            // TODO: send a cancel join request to the server, don't do anything else
-        }
-
-        private void PlayWord(string word)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// If the game window is exited, hides the window instead of closing
-        /// and brings up the start window for a new game.
-        /// </summary>
-        private void ExitGame(FormClosingEventArgs e)
-        {
-            if (e.CloseReason == CloseReason.UserClosing)
-            {
-                e.Cancel = true;
-                gameWindow.HideWindow();
-                startWindow.Show();
-            }            
-        }
-
-        /// <summary>
-        /// Returns a client with the given url as its base address
-        /// </summary>
-        private HttpClient CreateClient(string url)
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri(url);
-
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Add("Accept", "application/json");
-
-            return client;
+            }
         }
 
         /// <summary>
@@ -126,7 +90,76 @@ namespace BoggleClient
                 {
                     return "";
                 }
-            }            
+            }
+        }
+
+        /// <summary>
+        /// Requests the server to join a game using the given user token and specified duration
+        /// </summary>
+        private async Task<string> JoinGame(string userToken, int duration)
+        {
+            using (HttpClient client = CreateClient(startWindow.ServerUrl))
+            {
+                // Create the data for the request
+                dynamic data = new ExpandoObject();
+                data.UserToken = userToken;
+                data.TimeLimit = duration;
+
+                // Send POST request
+                StringContent content = new StringContent(JsonConvert.SerializeObject(data));
+                content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+                HttpResponseMessage response = await client.PostAsync("/BoggleService.svc/games", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Return the unique user token from the response
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject(responseContent);
+                    return result.GameID;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+        }
+
+        private async void CancelGameSearch()
+        {
+            // TODO: send a cancel join request to the server, don't do anything else
+        }
+
+        private void PlayWord(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// If the game window is exited, hides the window instead of closing
+        /// and brings up the start window for a new game.
+        /// </summary>
+        private void ExitGame(FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                gameWindow.HideWindow();
+                startWindow.Show();
+            }
+        }
+
+        /// <summary>
+        /// Returns a client with the given url as its base address
+        /// </summary>
+        private HttpClient CreateClient(string url)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            return client;
         }
     }
 }
