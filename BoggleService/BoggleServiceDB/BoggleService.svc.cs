2e79f6de1813f9b3fe1549cb55a1ce83e5fa397c
JeeDragon <bra_nker@hotmail.com>
2016-04-03 00:25:54 -0600

    Updated .gitignore file and added the .mdf and .ldf files back into the app folder.

--- BoggleService/BoggleServiceDB/BoggleService.svc.cs
+++ BoggleService/BoggleServiceDB/BoggleService.svc.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.IO;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
+using System.Net;
+using static System.Net.HttpStatusCode;
 using System.ServiceModel.Web;
-using System.Text;
+
 
 namespace BoggleServiceDB
 {
@@ -13,31 +13,107 @@ namespace BoggleServiceDB
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class BoggleService : IBoggleService
     {
-        public Stream API()
+        // Boggle Service Connection String
+        private static string BoggleServiceCS;
+
+        static BoggleService()
         {
-            throw new NotImplementedException();
+            BoggleServiceCS = ConfigurationManager.ConnectionStrings["BoggleDB"].ConnectionString;
         }
 
+        /// <summary>
+        /// Sets the status code for the next HTTP response.
+        /// </summary>
+        private static void SetStatus(HttpStatusCode code)
+        {
+            WebOperationContext.Current.OutgoingResponse.StatusCode = code;
+        }
+
+        public Stream API()
+        {
+            SetStatus(OK);
+            WebOperationContext.Current.OutgoingResponse.ContentType = "text/html";
+            return File.OpenRead(AppDomain.CurrentDomain.BaseDirectory + "index.html");
+        }
+        // Put
         public void CancelJoin(User user)
         {
             throw new NotImplementedException();
         }
 
-        public UserResponse CreateUser(User user)
+        // Post
+        public UserResponse CreateUser(User requestedUser)
         {
-            throw new NotImplementedException();
-        }
+            // Error check users information before setting up SQL connection.
+            if (requestedUser.Nickname == null || requestedUser.Nickname.Trim() == "")
+            {
+                SetStatus(Forbidden);
+                return null;
+            }
 
+            // Open SQL connection to BoggleDB
+            using (SqlConnection conn = new SqlConnection(BoggleServiceCS))
+            {
+                conn.Open();
+
+                // Open SQL transaction and begin transaction
+                using (SqlTransaction trans = conn.BeginTransaction())
+                {
+                    // Open SQL command with SQL code
+                    using (SqlCommand command = new SqlCommand("insert into Users(UserToken, Nickname) values(@UserToken, @Nickname)", conn, trans))
+                    {
+                        string userToken = Guid.NewGuid().ToString();
+                        // Set command parameters with AddWithValue
+                        command.Parameters.AddWithValue("@UserToken", userToken);
+                        command.Parameters.AddWithValue("@Nickname", requestedUser.Nickname.Trim());
+
+                        // Execute a non query and if successful set status code
+                        // We may not need to execute non query because we already know the nickname is 
+                        // correct and that the userToken will be correct.
+                        command.ExecuteNonQuery();
+                        SetStatus(Created);
+
+                        // Create formatted response to send back
+                        var response = new UserResponse();
+                        response.UserToken = userToken;
+
+                        // Commit users information into the database
+                        trans.Commit();
+                        return response;
+                    }
+                }
+
+            }
+        }
+        // Get
         public BoggleGameResponse GameStatus(string gameID, string brief)
         {
+
+            // Error check gameID and Brief information before setting up SQL connection.
+
+            // Open SQL connection to BoggleDB
+
+            // Open SQL transaction and begin transaction
+
+            // Open SQL command with SQL code
+
+            // Set command parameters with AddWithValue
+
+            // Use SqlDataReader reader = command.ExecuteReader to get information of of database.
+
+            // Execute a non query and if successful set status code
+
+            // Commit the transaction and return the UserResponse.
+
             throw new NotImplementedException();
         }
 
+        // Post
         public BoggleGameResponse JoinGame(JoinGameRequest requestBody)
         {
             throw new NotImplementedException();
         }
-
+        // Put
         public BoggleWordResponse PlayWord(string gameID, BoggleWord word)
         {
             throw new NotImplementedException();
