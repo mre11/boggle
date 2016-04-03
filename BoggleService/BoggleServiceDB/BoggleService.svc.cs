f932d263818830267e4bc532ac3c970d76307852
JeeDragon <bra_nker@hotmail.com>
2016-04-03 01:06:11 -0600

    Changed namespace back to Boggle

--- BoggleService/BoggleServiceDB/BoggleService.svc.cs
+++ BoggleService/BoggleServiceDB/BoggleService.svc.cs
@@ -7,7 +7,7 @@ using static System.Net.HttpStatusCode;
 using System.ServiceModel.Web;
 
 
-namespace BoggleServiceDB
+namespace Boggle
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
